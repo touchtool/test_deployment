8989792de14d9c5f5a2f7809c5dfa4162781a639
natchanon-space <natchanon.space@gmail.com>
2021-10-11 23:55:40 +0700

    #1 create django models

--- skdue_calendar/admin.py
+++ skdue_calendar/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Calendar, CalendarEvent
 
-# Register your models here.
+admin.site.register(Calendar)
+admin.site.register(CalendarEvent)
--- skdue_calendar/models.py
+++ skdue_calendar/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}'
+
+    def generate_slug(self, name: str):
+        return '-'.join(name.lower().split())
+
+
+class CalendarEvent(models.Model):
+    calendar = models.ForeignKey(Calendar, related_name="calendars", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-start_date',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.calendar.slug}/{self.slug}'
+
+    def generate_slug(self, name: str):
+        return '-'.join(name.lower().split())
