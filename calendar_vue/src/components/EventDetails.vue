52d07b000c30341661adb171ddfc0fc4bea44da9
patkamon <patkamon.a@ku.th>
2021-10-14 00:39:40 +0700

    #3 detail show up on top after click events

---
+++ calendar_vue/src/components/EventDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <transition name="modal-animation">
+      <div v-show="modalActive" class="modal">
+          <transition name="modal-animation-inner">
+              <div v-show="modalActive" class="modal-inner">
+                <!--close icon-->
+                <i class="fas fa-times"></i>
+                <!-- Modal Content-->
+                <slot/>
+                <button>Close</button>
+
+              </div>
+          </transition>
+      </div>
+  </transition>
+</template>
+
+<script>
+export default {
+props: ["modalActive"],
+
+};
+</script>
+
+<style >
+</style>
