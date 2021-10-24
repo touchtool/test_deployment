888988fe537ffd974d5c3b7baa0e03511facdbd9
Kittison Jackthreemongkol <78125325+touchtool@users.noreply.github.com>
2021-10-24 19:29:27 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+nxfbt7qx$0j+=r*708#1nngm2rdy%&u7=p!x-42q2v)7g7nbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
