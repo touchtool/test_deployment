86a8e258b53273dc4f0e53db09f14791b474f348
Kittison Jackthreemongkol <78125325+touchtool@users.noreply.github.com>
2021-10-24 19:30:58 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,12 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
